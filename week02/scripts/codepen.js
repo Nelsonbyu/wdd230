016c6278176da3f65fe27a565210019dbea6c412
Nelsonbyu <sabastiantumwesige@gmail.com>
2024-04-30 12:33:55 +0300

    week 02

--- week02/scripts/codepen.js
+++ week02/scripts/codepen.js
@@ -1,7 +1,7 @@
-const hamButton = document.querySelector('#menu');
+const Button = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
 
-hamButton.addEventListener('click', () => {
+Button.addEventListener('click', () => {
 	navigation.classList.toggle('show');
 	hamButton.classList.toggle('open');
 });
