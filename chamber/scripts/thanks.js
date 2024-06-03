4f3587cf9bb96cf91d22afc9b669c684c03e9e45
Nelsonbyu <sabastiantumwesige@gmail.om>
2024-06-04 02:07:46 +0300

    thankyou

---
+++ chamber/scripts/thanks.js
@@ -0,0 +1,17 @@
+//getting current year and date 
+
+//current year
+var currentYear = new Date().getFullYear();
+//last modification
+var lastModifiedDate = document.lastModified;
+//getting information from footer(in HTML)
+var content = document.getElementById('content');
+var more = document.getElementById('more');
+var lastModified = document.getElementById('lastModified');
+
+//getting the copyright and the name
+content.innerHTML = "&copy; " + currentYear + " " + "&#9829" + " "+ "LianTech Chamber of commerce, Uganda" + " " + "&#9829";
+more.innerHTML = "WDD 230, " + "Web Frontend Development 1 ";
+
+//modification output
+lastModified.textContent = "Last Modification: " + lastModifiedDate;
