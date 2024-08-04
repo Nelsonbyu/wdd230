c55aac8ee033e58442c49c9947d5ee442b4e974a
Nelsonbyu <sabastiantumwesige@gmail.com>
2024-08-04 14:40:40 +0300

    NEW

---
+++ Python Work/calc.py
@@ -0,0 +1,17 @@
+# Project: fuctions of some MTN code services Uganda
+# Developer: Tumwesige Nelson
+
+import math 
+
+print("""
+Welcome to MTN services:
+    1. *185#
+    2. *160*45#
+    3. *165#
+    5. *146#`
+    6. *160#
+    7. *150#
+      """)
+choice = input("choose code... ")
+while choice == "chioice":
+    print(choice)
---
+++ WDD230 CLASS WORK 2024/MY PROJECTS/mtncodes.py
@@ -0,0 +1,20 @@
+import math
+
+# PROJECT:MTN codes
+# DEVELOPER:Tumwesige Nelson
+#==
+
+#calling for possible mtn codes
+print()
+print(
+    """
+Some of the MTN codes.....
+1. *185# 
+2. *160#
+3. *150#
+4. *165#
+5. *146#
+"""
+)
+
+choice = input("Choose a code for your needed services: ")
