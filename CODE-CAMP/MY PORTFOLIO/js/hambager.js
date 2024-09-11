1ce90f069c2f64842e5b9540e74cc2fbdb5a115c
Nelsonbyu <sabastiantumwesige@gmail.om>
2024-09-12 00:51:19 +0300

    portifolio

--- CODE-CAMP/MY PORTFOLIO/js/hambager.js
+++ CODE-CAMP/MY PORTFOLIO/js/hambager.js
@@ -1,9 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const hamburger = document.getElementById('hamburger');
-    const navLinks = document.getElementById('nav-links');
-  
-    hamburger.addEventListener('click', function() {
-      navLinks.classList.toggle('active');
-    });
-  });
+const hamburger = document.querySelector(".hamburger")
+const navMenu = document.querySelector(".nav-menu")
+
+//OPENING AND CLOSING OF THE HAMBURGER
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+//CLOSING OF THE MENU AFTER A SELECTION
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
   
---
+++ JAVASCRIPT/index.js
@@ -0,0 +1,12 @@
+
+  //object
+  let person = {
+    name: 'Nelson',
+    age: 24
+  };
+// dot notation
+  person.age = 34;
+
+  //bracket notastion
+  person[name] = 'John';
+  console.log(person[name])
